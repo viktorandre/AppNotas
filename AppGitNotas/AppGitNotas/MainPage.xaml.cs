59a5de9a9b43a70da06aec5de50f7cbfb9d83e51
viktorandre <67350958+viktorandre@users.noreply.github.com>
2020-06-23 22:05:49 -0500

    Proyecto actualizado

--- AppGitNotas/AppGitNotas/MainPage.xaml.cs
+++ AppGitNotas/AppGitNotas/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+//Agregar
+using System.IO;
 
 namespace AppGitNotas
 {
@@ -13,9 +15,45 @@ namespace AppGitNotas
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        //Obtener el directorio local para almacenar el archivo notas.txt
+        string archivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notas.txt");
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(archivo))
+            {
+                txteditor.Text = File.ReadAllText(archivo);
+            }
+        }
+
+        private void btnGrabar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(archivo, txteditor.Text);
+                DisplayAlert("Exito", "Archivo grabado con exito", "OK");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(archivo))
+            {
+                try
+                {
+                    File.Delete(archivo);
+                    txteditor.Text = string.Empty;
+                    DisplayAlert("Exito", "Archivo eliminado con exito", "OK");
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
         }
     }
 }
--- AppGitNotas/AppGitNotas/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ AppGitNotas/AppGitNotas/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,20 @@ namespace AppGitNotas {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Editor txteditor;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnGrabar;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnEliminar;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            txteditor = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Editor>(this, "txteditor");
+            btnGrabar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnGrabar");
+            btnEliminar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnEliminar");
         }
     }
 }
